a10df2c8d498f934f341ea75cee35e2bb2389842
pradyumn8 <pradyumnv8@gmail.com>
2024-02-18 13:59:55 +0530

    counter Application is work properly

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -2,10 +2,49 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
-  return (
-    <h1>Namaste React 🙏</h1>
-  )
+
+const getButtonText =() => {
+  if (count === 21) {
+    return 'Reset for Zero';
+  } else {
+    return 'Increment: ';
+  }
+};
+
+const handleIncrement = () => {
+  if (count === 21) {
+    setCount(0);
+  } else {
+    setCount(count+1)
+  }
+};
+
+const handleDecrement = () => {
+  if(count > 0) {
+    setCount(count-1);
+  }
+}
+return (
+  <>
+ <h1>Namaste React🙏</h1>
+      <div className="card">
+        <button onClick={handleIncrement}>
+          {getButtonText()} {count === 21 ? '' : count}
+        </button>
+        <p>
+          <button onClick={handleDecrement}>
+            Remove: {count}
+          </button>
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Display Value: {count}
+      </p>
+      </>
+)
+
 }
 
 export default App
