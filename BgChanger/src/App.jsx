4f1dbd6802d972e5bb8a19c50839373827a9a6f1
pradyumn8 <pradyumnv8@gmail.com>
2024-02-19 13:52:22 +0530

    BgChanger is ready with Tailwind

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -14,6 +14,22 @@ function App() {
       <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
          <button onClick={() => setColor('violet')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'violet'}}>Violet</button>
          
+    
+         <button onClick={() => setColor('indigo')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'indigo'}}>Indigo</button>
+         
+         <button onClick={() => setColor('blue')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'blue'}}>Blue</button>
+         
+         <button onClick={() => setColor('green')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'green'}}>Green</button>
+         
+         <button onClick={() => setColor('yellow')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: 'yellow'}}>Yellow</button>
+         
+         <button onClick={() => setColor('orange')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'orange'}}>Orange</button>
+         
+         
+         <button onClick={() => setColor('red')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'red'}}>Red</button>
+         
+         <button onClick={() => setColor('pink')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: 'pink'}}>Pink</button>
+         
       </div>
     </div>
   </div>
