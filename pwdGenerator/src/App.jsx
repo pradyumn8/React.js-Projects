eca82a078985e8876bf654ab6a032dac3dd250f7
pradyumn8 <pradyumnv8@gmail.com>
2024-02-20 16:10:09 +0530

    pass 8 in useState for default get 8 character password

--- pwdGenerator/src/App.jsx
+++ pwdGenerator/src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+    if (numberAllowed) str += '0123456789'
+    if(charAllowed) str += '!@#$%^&*-_+=[]{}~`'
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+  
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
 
   return (
-  <div className="flex items-center justify-center h-screen" >
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3  bg-gray-800 text-orange-500">
-      <h1 className="text-white text-center my-3">Password Generator</h1>
+  <div className="flex items-center bg-indigo-200 justify-center h-screen" >
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 bg-gray-800 text-orange-500 ">
+      <h1 className="text-white text-3xl text-center my-5">Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
         type="text"
+        value={password}
         className='outline-none w-full py-1 px-3'
         placeholder='Password'
         readOnly
