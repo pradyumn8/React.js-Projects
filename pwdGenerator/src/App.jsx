d8eae4bd42fdcc744b8c28bf1981df64d24bbc80
pradyumn8 <pradyumnv8@gmail.com>
2024-02-20 15:42:56 +0530

    UI is ready for password Generator

--- pwdGenerator/src/App.jsx
+++ pwdGenerator/src/App.jsx
@@ -5,7 +5,43 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <h1>Namsate</h1>
+  <div className="flex items-center justify-center h-screen" >
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3  bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+        type="text"
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password'
+        readOnly
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-900'>copy</button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+          type="range"
+          min={6}
+          max={100}
+          className='cursor-pointer'
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+          type="checkbox"
+    
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+          type="checkbox" />
+        <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
