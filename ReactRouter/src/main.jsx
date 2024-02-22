5e7fe5005604a2cd603b01dc3bd121a0a0d3e92c
pradyumn8 <pradyumnv8@gmail.com>
2024-02-22 14:19:36 +0530

    Basic Structure of ReactRouter is done using Tailwind

---
+++ ReactRouter/src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
+import App from "./App"
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+import Layout from "./Layout"
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
+import Contact from "./components/Contact/Contact"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
