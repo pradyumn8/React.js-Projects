137542a4a9463c51ce26425b9b82ae30c1e12ca8
pradyumn8 <pradyumnv8@gmail.com>
2024-02-23 12:30:42 +0530

    Basic UI of component is completed

---
+++ MiniContext/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Login() {
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type="text"
+        placeholder='username' />
+        <input type="text"
+        placeholder='password' />
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Login
