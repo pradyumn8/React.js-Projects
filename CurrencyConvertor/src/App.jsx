6a52af7c85c03a56c862ac99af084d022222e36c
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-10-24 13:19:38 +0530

    Update code for currency convertor

--- CurrencyConvertor/src/App.jsx
+++ CurrencyConvertor/src/App.jsx
@@ -1,6 +1,5 @@
-// App.js
 import React, { useState } from 'react';
-import { InputBox } from './components';
+import InputBox from './components/InputBox';
 import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
         setTo(from);
         setConvertedAmount(amount);
         setAmount(convertedAmount);
-    }
+    }   
 
     const convert = () => {
         setConvertedAmount(amount * currencyInfo[to]);
@@ -26,9 +25,14 @@ function App() {
     return (
         <div className="container bg-slate-800 mx-auto flex flex-wrap items-center justify-center h-screen">
             <div className="w-full h-full bg-gradient-to-tl from-pink-300 to-indigo-300 md:w-1/2">
-                
-                <img src="https://images.unsplash.com/photo-1518183214770-9cffbec72538?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="Currency Exchange" className="w-full h-full object-cover mix-blend-overlay absolute" />
-                <div className="text-6xl font-bold flex justify-center items-center h-screen text-blue-800 underline underline-offset-5">Currency Convertor</div>
+                <img
+                    src="https://images.unsplash.com/photo-1518183214770-9cffbec72538?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                    alt="Currency Exchange"
+                    className="w-full h-full object-cover mix-blend-overlay absolute"
+                />
+                <div className="text-6xl font-bold flex justify-center items-center h-screen text-blue-800 underline underline-offset-5">
+                    Currency Convertor
+                </div>
             </div>
             <div className="w-full md:w-1/2 p-4">
                 <div className="max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
